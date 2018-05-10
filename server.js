3a293e8d9203ef3caceb12aa4814fc67d678354d
bvreeman <bvreeman@gmail.com>
2018-05-10 14:49:19 -0500

    got the questions set up in the survey

--- server.js
+++ server.js
@@ -3,3 +3,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+const apiRoutes = require('./app/routing/apiRoutes.js')(app);
+const htmlRoutes = require('./app/routing/htmlRoutes.js')(app);
+
+app.use('/', htmlRoutes);
+app.use('/', apiRoutes);
+
+app.listen(PORT, function() {
+  console.log(`App listening on PORT ${PORT}`);
+});
