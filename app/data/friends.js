6d5870c0bea028608d33fb58a8b32908d8620bf7
bvreeman <bvreeman@gmail.com>
2018-05-10 14:20:15 -0500

    added all the data to the character list

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+const characterList = [{
+  name: 'George',
+  photo: 'http://www.brandonvreeman.com/wp-content/uploads/2017/03/George-Large-PNG-300x293.png',
+  scores: [5, 4, 3, 4, 5, 1, 3, 4, 3, 3, 1, 3, 3, 2, 4, 3, 2, 3, 5, 3],
+},
+{
+  name: 'Fardinand',
+  photo: 'https://vignette.wikia.nocookie.net/ferdinandmovie/images/e/e2/Ferdinand_and_logo.png/revision/latest?cb=20171128103501',
+  scores: [5, 5, 3, 5, 1, 5, 4, 3, 3, 1, 5, 4, 4, 3, 2, 4, 2, 5, 4],
+},
+{
+  name: 'Little Engine That Could',
+  photo: 'http://www.buttonmuseum.org/sites/default/files/EN-the-little-engine-that-could-button-busy-beaver-button-museum.png',
+  scores: [5, 5, 2, 2, 4, 1, 2, 4, 3, 1, 1, 3, 3, 3, 2, 2, 3, 3, 5, 2],
+},
+{
+  name: 'Pete the Cat',
+  photo: 'http://assets.petethecatbooks.com/wp-content/uploads/2015/09/08182428/icanread-icon.png',
+  scores: [2, 2, 3, 5, 1, 4, 5, 4, 3, 3, 3, 4, 4, 2, 4, 2, 3, 5, 3, 3],
+},
+{
+  name: 'Cat in the Hat',
+  photo: 'https://vignette.wikia.nocookie.net/seuss/images/3/3a/Cat_in_hat_character1.png/revision/latest?cb=20140301070354',
+  scores: [4, 4, 5, 5, 3, 5, 5, 4, 4, 3, 2, 3, 5, 4, 5, 4, 5, 3, 4, 5],
+},
+{
+  name: 'Harry Potter',
+  photo: 'http://www.freepngimg.com/download/harry_potter/5-2-harry-potter-png-file.png',
+  scores: [4, 4, 5, 1, 4, 2, 2, 3, 4, 3, 3, 3, 4, 4, 4, 4, 4, 3, 5, 3],
+},
+{
+  name: 'Junie B. Jones',
+  photo: 'https://orig00.deviantart.net/c598/f/2017/080/2/d/junie_b__jones_by_figyalova-db3321d.png',
+  scores: [5, 4, 3, 2, 4, 3, 3, 5, 4, 3, 5, 4, 5, 5, 4, 4, 4, 2, 5, 4],
+},
+{
+  name: 'Winnie the Pooh',
+  photo: 'http://www.pngmart.com/files/4/Winnie-The-Pooh-PNG-Picture.png',
+  scores: [3, 4, 2, 3, 2, 2, 5, 3, 4, 5, 1, 4, 2, 2, 2, 2, 2, 4, 3, 4],
+},
+{
+  name: 'Peter Rabbit',
+  photo: 'https://1.bp.blogspot.com/-PLRviE6bMPM/WnvfxaqVufI/AAAAAAABPB8/93TImy3SK0wiheUozbMPKdc52xv_9JbiwCKgBGAs/s1600/5a0ac1585a997e1c2cea1092.png',
+  scores: [3, 3, 1, 2, 3, 2, 5, 4, 1, 3, 2, 2, 2, 5, 2, 5, 5, 2, 3, 2],
+},
+{
+  name: 'Fancy Nancy',
+  photo: 'http://d2v7ugaofxr6z8.cloudfront.net/wp-content/uploads/2016/12/30093140/picture-books-trim1.png',
+  scores: [5, 4, 5, 4, 2, 5, 4, 4, 5, 4, 5, 4, 5, 3, 3, 2, 3, 3, 4, 5],
+}];
+
+// for (let i = 0; i < characterList.length; i++)
+// { console.log(`\n${characterList[i].name}\n${characterList[i].photo}`); }
+
+module.exports = characterList;
